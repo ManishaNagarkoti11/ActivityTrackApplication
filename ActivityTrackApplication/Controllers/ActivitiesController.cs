dc071aaaf573807d0b0560cdb9e33af2ca298d6c
Manisha Nagarkoti <Manisha.nagarkoti40@outlook.com>
2022-07-06 16:16:37 +0545

    Add project files.

---
+++ ActivityTrackApplication/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActivityTrackApplication.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ActivitiesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> ActivitiesList()
+        {
+            return await _dbContext.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityById(Guid id)
+        {
+            return await _dbContext.Activities.FindAsync(id);
+        }
+    }
+}
